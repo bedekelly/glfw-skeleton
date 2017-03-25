43275df1ce9da6ea03462bead30365b204215828
Bede Kelly <bedekelly97@gmail.com>
2017-03-25 17:03:09 +0000

    Window fades between colours, thanks to new opengl context

--- src/skeleton_utils.cpp
+++ src/skeleton_utils.cpp
@@ -23,8 +23,10 @@ GLFWwindow* createWindow(const char* title) {
   if (!glfwInit())
     exit(EXIT_FAILURE);
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   window = glfwCreateWindow(640, 480, title, NULL, NULL);
   if (!window)
