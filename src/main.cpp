d5b971998b78a6238620219a42021fae3119cd83
Bede Kelly <bedekelly97@gmail.com>
2017-03-25 17:03:09 +0000

    Window fades between colours, thanks to new opengl context

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <math.h>
 #include <linmath.h>
 
 #include "skeleton_utils.h"
+#include "main.h"
 
 
 int main(int argc, char **argv) {
@@ -10,9 +13,10 @@ int main(int argc, char **argv) {
   glClear(GL_COLOR_BUFFER_BIT);
 
   while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    // Do something cool!
+    // Render a colour based on the current time.
+    double time = glfwGetTime();
+    render(time);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -20,3 +24,21 @@ int main(int argc, char **argv) {
 
   cleanExit(window);
 }
+
+
+/**
+ * Given a current time, calculate a colour and "clear" the screen
+ * to that colour.
+ */
+void render(double currentTime) {
+  static double TWO_PI = 3.14159 * 2;
+  const GLfloat color[] = {
+    (float) sin(currentTime) * 0.5f + 0.5f,  // Red
+    (float) cos(currentTime) * 0.5f + 0.5f,  // Green
+    0.0f,                                    // Blue
+    1.0f                                     // Alpha
+  };
+  glClearBufferfv(GL_COLOR, 0, color);
+}
+
+
